8127c596e67e0cb99ec8336ec6c883bb6a042387
kienk93 <kienk93@users.noreply.github.com>
2021-04-08 14:09:05 +0700

    'add'

---
+++ mypack/B.java
@@ -0,0 +1,20 @@
+//save by B.java  
+
+package mypack;
+
+import pack.*;
+
+
+
+class B extends A{
+
+    public static void main(String args[]){
+
+        B obj = new B();
+
+        obj.msg();
+
+    }
+
+}
+
